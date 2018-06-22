e0eb363d7a054c46b8fec00698063a92949cacf8
daichi.hirota.ts <daichi.hirota.ts@20202984n.local>
2018-06-22 18:38:19 +0900

    second commit

---
+++ src/plugins/element.js
@@ -0,0 +1,6 @@
+import Vue from 'vue';
+import ElementUI from 'element-ui';
+import locale from 'element-ui/lib/locale/lang/ja';
+// import '@/assets/element.scss';
+
+Vue.use(ElementUI, { locale });
